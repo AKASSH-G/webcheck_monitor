961dd17de997192a695578a66b95920b500d6383
akassh <20i105@kce.ac.in>
2023-07-19 20:43:16 +0530

    firstcommit

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import datetime
-app = Flask(__name__)
-@app.route("/")
-def index1():
-    return {"time": datetime.datetime.now()}
-if __name__ == "__main__":
-    app.run()
